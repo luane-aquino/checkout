e93392ad0301d95dbab42af8a8900f60eb7f4c82
Luane Aquino <luane.aquino.br@gmail.com>
2023-12-11 21:23:31 +0900

    feat: added initial code for payment screen

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,13 +2,20 @@ import "./styles.scss";
 
 type ButtonType = {
   text: string;
-  handleClick: () => void;
+  handleClick?: () => void;
   isPrimary?: boolean;
+  type?: "button" | "submit" | "reset" | undefined;
 };
 
-const Button = ({ text, isPrimary = true, handleClick }: ButtonType) => {
+const Button = ({
+  text,
+  isPrimary = true,
+  handleClick,
+  type = "button",
+}: ButtonType) => {
   return (
     <button
+      type={type}
       onClick={handleClick}
       className={`button ${isPrimary ? "primary" : "secondary"}`}
     >
--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,11 +1,64 @@
+import { useForm } from "react-hook-form";
 import { TabContentItemEnum } from "../../common/types";
+import Card from "../../components/atoms/Card";
 import Tabs from "../../components/molecules/Tabs";
+import "./styles.scss";
+import Container from "../../components/atoms/Container";
+import PaymentPlan from "../../components/molecules/PaymentPlan";
+import { data } from "../../mock/data";
+import Button from "../../components/atoms/Button";
 
 const Payment = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      cardNumber: "",
+      cardHolderName: "",
+      cardValidUntil: "",
+      cvv: "",
+    },
+  });
   return (
     <>
       <Tabs path={TabContentItemEnum.payment} />
-      <h1>payment!</h1>
+      <form
+        onSubmit={handleSubmit((data) => {
+          alert(JSON.stringify(data));
+        })}
+        className="wrapper"
+      >
+        <Card>
+          <h1>Cartão de crédito</h1>
+          <label>Número</label>
+          <input {...register("cardNumber")} />
+          <label>Nome do titular do cartão</label>
+          <input
+            {...register("cardHolderName", { required: true, maxLength: 10 })}
+          />
+          <label>Data de validade</label>
+          <input
+            {...register("cardValidUntil", { required: true, maxLength: 10 })}
+          />
+          <label>Código CVV:</label>
+          <input {...register("cvv", { required: true, maxLength: 10 })} />
+          {/* <input type="submit" /> */}
+        </Card>
+        {/* prices */}
+        <Container>
+          <PaymentPlan
+            quantity={data.products.length}
+            total={data.paymentPlan.total}
+            shipping={data.paymentPlan.shipping}
+            discount={data.paymentPlan.discount}
+            subtotal={data.paymentPlan.subtotal}
+          />
+          <Button type="submit" text="Seguir para o pagamento" />
+        </Container>
+      </form>
     </>
   );
 };
