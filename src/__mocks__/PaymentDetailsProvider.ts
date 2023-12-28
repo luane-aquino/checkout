a3cb46dac56c33d5d2bc316b59ebd44b8f80ec24
Luane Aquino <luane.aquino.br@gmail.com>
2023-12-28 14:52:48 +0900

    test: added more tests

---
+++ src/__mocks__/PaymentDetailsProvider.ts
@@ -0,0 +1,1 @@
+export const usePayment = jest.fn();
--- src/components/molecules/Product/Product.test.tsx
+++ src/components/molecules/Product/Product.test.tsx
@@ -55,4 +55,31 @@ describe("Product", () => {
 
     expect(screen.getAllByTestId("price-old").length).toBe(1);
   });
+
+  it("should show product details without price", async () => {
+    const productsData = data as CartType;
+    render(
+      <Card>
+        <Product.Root>
+          {productsData?.products.map((item: ProductType, index: number) => (
+            <Product.Description
+              imageUrl={item.imageUrl}
+              description={item.description}
+              key={index}
+            />
+          ))}
+        </Product.Root>
+      </Card>,
+    );
+
+    const images = await screen.findAllByRole("img", { hidden: true });
+    expect(images.length).toBe(3);
+    expect(
+      screen.getByText(
+        "L'Oréal Professionnel Expert Absolut Repair Cortex Lipidium - Máscara de Reconstrução",
+      ),
+    ).toBeVisible();
+    expect(screen.queryByText("243,90")).not.toBeInTheDocument();
+    expect(screen.queryByText("R$ 225,90")).not.toBeInTheDocument();
+  });
 });
