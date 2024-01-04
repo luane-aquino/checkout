6400d9dc77bb9888e55b00e4755a9a2f17bbed0d
Luane Aquino <luane.aquino.br@gmail.com>
2024-01-04 14:02:06 +0900

    review: refactored routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-import ErrorPage from "./pages/ErrorPage";
-import Bag from "./pages/Bag";
-import Payment from "./pages/Payment";
-import ConfirmationSuccess from "./pages/ConfirmationSuccess";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PaymentDetailsProvider from "./store/PaymentDetailsProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CartProvider from "./store/CartProvider";
+import { routes } from "./routes";
 
 const App = () => {
   const queryClient = new QueryClient();
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navigate to="/bag" />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/bag",
-      element: <Bag />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/payment",
-      element: <Payment />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/confirmation",
-      element: <ConfirmationSuccess />,
-      errorElement: <ErrorPage />,
-    },
-  ]);
+  const router = createBrowserRouter(routes);
 
   return (
     <PaymentDetailsProvider>
