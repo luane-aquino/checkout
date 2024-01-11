c2ca1377b779723adbe2e85d43c648250732254d
Luane Aquino <luane.aquino.br@gmail.com>
2024-01-11 11:50:11 +0900

    review: using mocked api mocky.io

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,6 @@
 export const getProducts = async () => {
-  const res = await fetch("/mocks/productMock.json");
+  const res = await fetch(
+    "https://run.mocky.io/v3/8469a25f-59d8-4d55-9973-2d96ccabe6ed",
+  );
   return res.json();
 };
