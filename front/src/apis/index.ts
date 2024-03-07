77f810ae296bd642ca11823528b63a62f0465df3
Luane Aquino <luane.aquino.br@gmail.com>
2024-03-07 15:54:48 +0900

    feat: configured cors and consume data from back

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -1,6 +1,4 @@
 export const getProducts = async () => {
-  const res = await fetch(
-    "https://run.mocky.io/v3/8469a25f-59d8-4d55-9973-2d96ccabe6ed",
-  );
+  const res = await fetch("http://localhost:5000/cart");
   return res.json();
 };
