e441e9085ae51b10d7f44f70e8067a31782ad47a
Luane Aquino <luane.aquino.br@gmail.com>
2024-05-13 13:46:19 +0900

    review: put base url in env file

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -1,23 +1,19 @@
 import { OrderType } from "models/order";
 
 export const customerDocument = "12345678900";
+const base_url = process.env.BASE_URL;
 
 export const getProducts = async () => {
-  const res = await fetch(
-    `http://localhost:5000/api/customer/${customerDocument}/cart`,
-  );
+  const res = await fetch(`${base_url}/${customerDocument}/cart`);
   return res.json();
 };
 
 export const makeCheckout = async (order: OrderType) => {
-  const response = await fetch(
-    `http://localhost:5000/api/customer/${customerDocument}/checkout`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(order),
-    },
-  );
+  const response = await fetch(`${base_url}/${customerDocument}/checkout`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(order),
+  });
 
   if (!response.ok) {
     throw await response.json();
