8563318e994fe5c83c440be9c2996265fb55fd09
Luane Aquino <luane.aquino.br@gmail.com>
2024-05-19 21:13:01 +0900

    deploy(back): configured to run api in vercel

--- back/index.ts
+++ back/index.ts
@@ -57,3 +57,5 @@ app.post("/api/customer/:document/checkout", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+export default app;
